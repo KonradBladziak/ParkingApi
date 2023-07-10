5112763ec6085501c187599d25db63de15444b38
Konrad Bladziak <konrbla262@student.polsl.pl>
2023-07-10 21:56:56 +0200

    Wstep do wstepu

--- ParkingApi/WebApi/Controllers/MiastaController.cs
+++ ParkingApi/WebApi/Controllers/MiastaController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DAL.Entity;
+using DAL.UnitOfWork;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
@@ -7,5 +9,40 @@ namespace WebApi.Controllers
     [ApiController]
     public class MiastaController : ControllerBase
     {
+        private IUnitOfWork unitOfWork;
+
+        public MiastaController(IUnitOfWork unitOfWork) 
+        { 
+            this.unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("GetWszyskieMiasta")]
+        public async Task<IActionResult> GetAllMiasta() { 
+        
+            var res = unitOfWork.MiastoRepository.GetAllAsync().Result.ToList();
+
+            return Ok(res);
+        }
+
+        [HttpGet("GetMiastoById/{id?}")]
+        public async Task<IActionResult> GetMiastoById(int id)
+        {
+            var res = unitOfWork.MiastoRepository.GetByIdAsync(id).Result;
+
+            return Ok(res);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> AddMiasto(string nazwa,string wojewodztwo) {
+
+            var miasto = new Miasto {Nazwa = nazwa,Wojewodztwo = wojewodztwo };
+
+            unitOfWork.MiastoRepository.AddAsync(miasto);
+
+            return Ok(miasto);
+        
+        }
+
+
     }
 }
--- ParkingApi/WebApi/Program.cs
+++ ParkingApi/WebApi/Program.cs
@@ -15,7 +15,6 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
