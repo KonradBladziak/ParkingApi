ca1ccf911d3053fe3fcdb1bbb8295cfd97111c64
Konrad Bladziak <konrbla262@student.polsl.pl>
2023-07-11 18:02:34 +0200

    ParkingRepo

---
+++ ParkingApi/DAL/IRepository/IParkingRepository.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.IRepository
+{
+    public interface IParkingRepository : IRepository<Parking>
+    {
+
+        Task<IEnumerable<Parking>> GetAllAsync();
+        Task<Parking> GetByIdAsync(int id);
+
+        void Add(Parking parking);
+
+        void Delete(Parking parking);
+
+        void Update(Parking parking);
+    }
+}
---
+++ ParkingApi/DAL/Repository/ParkingRepository.cs
@@ -0,0 +1,42 @@
+﻿using DAL.DataContext;
+using DAL.Entity;
+using DAL.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    public class ParkingRepository :Repository<Parking>, IParkingRepository
+    {
+    public ParkingRepository(DatabaseContext databaseContext) : base(databaseContext) { }
+
+    public async Task<IEnumerable<Parking>> GetAllAsync()
+    {
+        return await FindAll().OrderBy(x => x.Nazwa).ToListAsync();
+    }
+
+    public async Task<Parking> GetByIdAsync(int id)
+    {
+        return await FindByCondition(x => x.Id.Equals(id)).FirstOrDefaultAsync();
+    }
+
+    public async Task Add(Parking parking)
+    {
+        Add(parking);
+    }
+
+    public async Task Delete(Parking parking)
+    {
+        Delete(parking);
+    }
+
+    public async Task Update(Parking parking)
+    {
+        Update(parking);
+    }
+
+}
